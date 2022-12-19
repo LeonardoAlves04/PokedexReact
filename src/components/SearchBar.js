6b2e9b939f02bee5538c3f401d003dc26954ea8a
Leonardo Alves <leonardoalves201510@gmail.com>
2022-12-19 17:32:22 -0300

    feat(): navbar quase pronta, preparando o código pra api

---
+++ src/components/SearchBar.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const SearchBar = () => {
+    const [search, setSearch] = useState("dito")
+
+    const onChangeHandler = (e) => {
+        console.log("Pokemon: ", e.target.value)
+        setSearch(e.target.value);
+    }
+    const onButtonClickHandler = () => {
+        console.log("pokemon: ", search)
+    }
+    return (
+        <div className="searchbar-container">
+            <div className="searchbar">
+                <input placeholder="Buscar pokemon" onChange={onChangeHandler} />
+            </div>
+            <div className="searchbar-btn">
+                <button onClick={onButtonClickHandler}> Buscar </button>
+            </div>
+        </div>
+    )
+}
+
+export default SearchBar;
---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div >
+                <img className="navbar-img" src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" alt="pokeapilogo" />
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
