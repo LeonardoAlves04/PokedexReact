3a041c994b9b6782b785617b31610a7b2b1853c1
Leonardo Alves <leonardoalves201510@gmail.com>
2022-12-26 15:44:52 -0300

    feat(): desenvolvendo a exibição de todos os pokemons (com nome, peso, foto)

--- src/App.js
+++ src/App.js
@@ -3,16 +3,33 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Pokedex from './components/Pokedex';
 import SearchBar from './components/SearchBar';
+import { getPokemon } from './components/api';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [pokemons, setPokemons] = useState([]);
+
+  const fetchPokemons = async () => {
+    try {
+      setLoading(true)
+      const result = await getPokemon();
+      setPokemons(result);
+      setLoading(false);
+    } catch (error) {
+      console.log("fetch Pokemons error: ", error)
+    }
+  }
+
   useEffect(() => {
     console.log("carregou")
+    fetchPokemons()
   }, [])
+
   return (
     <div>
       <Navbar />
       <SearchBar />
-      <Pokedex />
+      <Pokedex pokemons={pokemons} loading={loading} />
     </div>
   );
 }
--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,14 +1,27 @@
 import React from "react";
 
-const Pokedex = () => {
+const Pokedex = (props) => {
+    const { pokemons, loading } = props;
     return (
         <div >
             <div className="pokedex-header">
                 <h1>Pokedex</h1>
                 <div>Paginação</div>
             </div>
+            {loading ? (<div>Carregando, guenta um cado ai...</div>)
+                : (<div className="pokedex-grind">
+                    {pokemons && pokemons.map((pokemon, index) => {
+                        return (
+                            <div>
+                                <div> Nome: {pokemon.name}</div>
+                                <img alt={pokemon.name} src={pokemon.url} />
+                            </div>
+                        );
+                    })}
+                </div>
+                )}
         </div>
-    )
-}
+    );
+};
 
 export default Pokedex;
